dbe610c578444dec130a428202ee7fe68993d05a
EaliseW <ealise.wang@gmail.com>
2026-01-20 14:57:50 +1100

    add client config in main.tf and RBAC for key vault instead of access policy

--- key_vault.tf
+++ key_vault.tf
@@ -1,5 +1,5 @@
 resource "azurerm_key_vault" "kv" {
-  name                        = "examplekeyvault"
+  name                        = "kvazproject01"
   location                    = azurerm_resource_group.rg.location
   resource_group_name         = azurerm_resource_group.rg.name
   enabled_for_disk_encryption = true
@@ -9,20 +9,5 @@ resource "azurerm_key_vault" "kv" {
 
   sku_name = "standard"
 
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-
-    key_permissions = [
-      "Get",
-    ]
-
-    secret_permissions = [
-      "Get",
-    ]
-
-    storage_permissions = [
-      "Get",
-    ]
-  }
+  enable_rbac_authorization = true
 }
