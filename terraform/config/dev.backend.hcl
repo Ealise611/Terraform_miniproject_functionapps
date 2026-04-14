0e35fff9871d2a676a45d1d98743cbb6963c5788
EaliseW <ealise.wang@gmail.com>
2026-04-14 14:30:27 +1000

    recreate all backend files for dev and prod, and change both pipelines

---
+++ terraform/config/dev.backend.hcl
@@ -0,0 +1,5 @@
+subscription_id      = "9ffff625-f8ac-424b-937a-b2a0258cbd79"
+resource_group_name  = "az-p-lrn-sbx-rgr-cicd-001"
+storage_account_name = "azplrnsbxstocicd001"
+container_name       = "azplrnsbxstotfstate001"
+key = dev/terraform.tfstate
--- terraform/main.tf
+++ terraform/main.tf
@@ -6,13 +6,7 @@ terraform {
       version = "~> 3.0"
     }
   }
-  backend "azurerm" {
-    subscription_id      = "9ffff625-f8ac-424b-937a-b2a0258cbd79"
-    resource_group_name  = "az-p-lrn-sbx-rgr-cicd-001"
-    storage_account_name = "azplrnsbxstocicd001"
-    container_name       = "azplrnsbxstotfstate001"
-    key                  = "terraform.tfstate"
-  }
+  backend "azurerm" {}
 }
 
 provider "azurerm" {
